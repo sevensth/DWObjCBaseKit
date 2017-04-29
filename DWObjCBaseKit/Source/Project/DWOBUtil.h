20e8738edc4bb770353140f79f4f04c06160282b
sven <shrimpeter@gmail.com>
2017-04-29 18:45:37 +0800

    Update project settings and upload an empty class.

---
+++ DWObjCBaseKit/Source/Project/DWOBUtil.h
@@ -0,0 +1,13 @@
+//
+//  DWOBUtil.h
+//  DWObjCBaseKit
+//
+//  Created by Sven Song on 29/04/2017.
+//  Copyright © 2017 dreamingwish.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWOBUtil : NSObject
+
+@end
---
+++ DWObjCBaseKit/Source/Project/DWOBUtil.m
@@ -0,0 +1,13 @@
+//
+//  DWOBUtil.m
+//  DWObjCBaseKit
+//
+//  Created by Sven Song on 29/04/2017.
+//  Copyright © 2017 dreamingwish.com. All rights reserved.
+//
+
+#import "DWOBUtil.h"
+
+@implementation DWOBUtil
+
+@end
